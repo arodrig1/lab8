ccc59d5a40d423ebcb8d283f21db24b114db745c
Alejandro Rodriguez <arodrig1@stanford.edu>
2014-02-26 23:02:46 +0000

    Added google analytics and tracking for likes

--- app.js
+++ app.js
@@ -38,8 +38,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.view);
-// Example route
-// app.get('/users', user.list);
+app.get('/grid', index.viewGrid);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
